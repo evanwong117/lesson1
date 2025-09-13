4bf5f622ee6e04edc730ed762c59bd04a4519fb0
evanwong117 <evanwong117@gmail.com>
2025-09-13 20:46:25 +0800

    print

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print("Hello World 123");
+# Printing a greeting message
 print("I am Evan Wong")
+# Printing my age
+print("I am 10 years old")
+# Printing a fun fact about myself
+print("I know how to play drums, clarinet and piano")
+# Perfroming and printing a simple calculation
+print("The result of 10/3 =", 10/3)
