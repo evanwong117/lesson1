032f3eef0b74eaf34deccafcb54d9d713e62252d
evanwong117 <evanwong117@gmail.com>
2025-09-20 21:05:31 +0800

    typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+name="evan"
+age=10
+is_student=True
+weight=40.3
+print("name=",name)
+print("datatype of name is " ,type(name))
+print("age=",age)
+print("datatype of age is ",type(age))
+print("is_student=", is_student)
+print("datatype of is_student ",type(is_student))
+print("weight=",weight)
+print("datatype of weight=",type(weight))
+print("\n after typecasting")
+age=str(age)
+print("age=",age)
+print("datatype of age is ",type(age))
+weight=int(weight)
+print("weight=",weight)
+print("datatype of weight=",type(weight))
