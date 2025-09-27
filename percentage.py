cdcdfcbe3649713c876f51a2227b7c5086130424
evanwong117 <evanwong117@gmail.com>
2025-09-27 21:06:46 +0800

    percentage.py

---
+++ percentage.py
@@ -0,0 +1,9 @@
+math=int(input("maths:"))
+english=int(input("english:"))
+science=int(input("science:"))
+german=int(input("german:"))
+sum=math+english+science+german
+print("sum of math, english, science and german",sum)
+perc=(sum/400)*100
+print(end="Percentage Mark=")
+print(perc)
