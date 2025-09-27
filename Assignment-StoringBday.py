d7719df2a0e4c8bf07f7293d2531dd784e35ed0d
evanwong117 <evanwong117@gmail.com>
2025-09-27 21:25:45 +0800

    Assignment-StoringBday.py

---
+++ Assignment-StoringBday.py
@@ -0,0 +1,15 @@
+boy1="Nat"
+boy2="Sean"
+boy3="Harry"
+boy4="Evan"
+boy5="Hinton"
+bday1="9 Jan"
+bday2="12 Feb"
+bday3="15 Apr"
+bday4="29 Jan"
+bday5="25 Dec"
+print(boy1,"'s birthday is on", bday1)
+print(boy2,"'s birthday is on", bday2)
+print(boy3,"'s birthday is on", bday3)
+print(boy4,"'s birthday is on", bday4)
+print(boy5,"'s birthday is on", bday5)
