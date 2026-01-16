b57bf2a17eab41061b150e1169059fb5b6c11bd8
evanwong117 <evanwong117@gmail.com>
2026-01-16 10:47:18 +0800

    polygon-turtle.py

---
+++ polygon-turtle.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.Screen().bgcolor("cyan")
+turtle.Screen().setup(600,600)
+polygon=turtle.Turtle()
+for i in range(7):
+    polygon.forward(100)
+    polygon.right(60)
+turtle.done()
+
--- pyramid.py
+++ pyramid.py
@@ -7,7 +7,7 @@ for i in range(n,0,-1):
 
 print("Half of pyramid (*)")
 n=int(input("Enter the number of rows: "))
-for i in range(1,n):
+for i in range(1,n+1):
     for j in range (i):
         print(i,end="")
     print()
