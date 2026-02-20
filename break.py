a39230999ef1d7423f86299aabbd5ad7ef209418
evanwong117 <evanwong117@gmail.com>
2026-02-20 13:17:56 +0800

    break.py

---
+++ break.py
@@ -0,0 +1,8 @@
+a=str(input("Please enter the letter: "))
+for i in a:
+    if i=="a" or i=="A":
+        print("a is present")
+        break
+    else:
+        print("a is not present")
+        
