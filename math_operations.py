7b6fe8645c3eea1ec54e38ff9981fb2ebb96035b
evanwong117 <evanwong117@gmail.com>
2026-02-28 19:01:55 +0800

    random_number.py

--- math_operations.py
+++ math_operations.py
@@ -1,5 +1,5 @@
 import math
-print('The Floor and Ceiling value of 23.56 are: ' + str(math.ceil(23.56)) + ', ', + str(math.floor(23.56)))
+print('The Floor and Ceiling value of 23.56 are: ' + str(math.ceil(23.56)) + ', ' + str(math.floor(23.56)))
 x=10
 y=-15
 print('The value of x after copying the sign from y is: ' + str(math.copysign(x,y)))
