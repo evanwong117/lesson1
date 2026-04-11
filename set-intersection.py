9260b1396c56b35197991c229b37bfe2fa9c7002
evanwong117 <evanwong117@gmail.com>
2026-04-11 18:58:44 +0800

    set-intersection.py

---
+++ set-intersection.py
@@ -0,0 +1,17 @@
+setx= {"green", "blue", "red"}
+sety = {"blue", "yellow", "pink"}
+print("Original set elemtns: ")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets: ")
+setz = setx.intersection(sety)
+print(setz)
+print("\nUnion of two said sets: ")
+setz = setx.union(sety)
+print(setz)
+print("\nDifference of two said sets: ")
+setz = setx.difference(sety)
+print(setz)
+print("\nSymmetric Difference of two said sets: ")
+setz = setx.symmetric_difference(sety)
+print(setz)
