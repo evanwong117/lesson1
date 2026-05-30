47f9e5cde8f8c0fdcec4466060243559a5765a53
evanwong117 <evanwong117@gmail.com>
2026-05-30 18:54:06 +0800

    Assignment-dog-class.py

---
+++ Assignment-dog-class.py
@@ -0,0 +1,11 @@
+class Dog:
+    species = "Shiba Inu"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+blu = Dog("Blu", 10)
+woo = Dog("Woo", 15)
+print("Blu is a {}".format(blu.species))
+print("Woo is also a {}".format(woo.species))
+print("{} is {} years old". format(blu.name, blu.age))
+print("{} is {} years old". format(woo.name, woo.age))
